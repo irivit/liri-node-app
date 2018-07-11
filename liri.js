498afba4e3ddfaa350b090d5ba9fd6f1f7386df1
irivi <copacies@gmail.com>
2018-07-10 23:14:51 -0500

    comment added

--- liri.js
+++ liri.js
@@ -7,104 +7,86 @@ var Spotify = require("node-spotify-api");
 var fs = require("fs");
 
 var nodeArgs = process.argv;
+
+var songOrMovie = "";
+for (var i = 3; i < nodeArgs.length; i++) {   ///this for loop will concatenate in only one string the name of the the song or the movie if that has more than one word
+    if (i > 3 && i < nodeArgs.length) {
+        songOrMovie +=  "+" + nodeArgs[i];
+    }
+    else {
+        songOrMovie += nodeArgs[i];
+    }
+}
+
 var oper = process.argv[2];
+var type = "track";
 
-
+//Creating the instanced of the objects
 var client = new Twitter(keys.twitter);
 var spotify = new Spotify(keys.spotify);
 
-console.log(process.argv);
-
+// this switch will call the correct function depending on the choice of the user 
 switch (oper) {
     case 'my-tweets': {
         tweet();
         break;
     }
     case 'spotify-this-song': {
-        var songName = "";
-        for (var i = 3; i < nodeArgs.length; i++) {
-            if (i > 3 && i < nodeArgs.length) {
-                songName = songName + "+" + nodeArgs[i]; 
-            }
-            else {
-                songName += nodeArgs[i];
-            }
-        }
-        if (songName == "") {
-            songName = "The Sign"
-            spotifySong(songName);
+        //if the user doesn't enter any song the system will show The Sign by default
+        if (songOrMovie == "") {
+            songOrMovie = "The Sign"
+            spotifySong(songOrMovie, type);
         } else {
-            spotifySong(songName);
+            spotifySong(songOrMovie, type); //if not will show info about the song calling the spotifySong function
         }
         break;
     }
 
     case 'movie-this': {
-
-        var movieName = "";
-
-        for (var i = 3; i < nodeArgs.length; i++) {
-            if (i > 3 && i < nodeArgs.length) {
-                movieName = movieName + "+" + nodeArgs[i];
-            }
-            else {
-                movieName += nodeArgs[i];
-            }
-        }
-        if (movieName == "") {
-            movieName = "Mr. Nobody"
-            movie(movieName);
-        } else {
-            movie(movieName);
+        //if the user doesn't enter any movie the system will show Mr. Nobody by default
+        if (songOrMovie == "") {
+            songOrMovie = "Mr. Nobody"
+            movie(songOrMovie);
+        } else {                  //if not will show info about the movie calling the movie function
+            movie(songOrMovie);
         }
         break;
     }
     case 'do-what-it-says': {
+        // in this case will read and execute a comand from the random.txt
+
         fs.readFile("random.txt", "utf8", function (error, data) {
 
             // If the code experiences any errors it will log the error to the console.
             if (error) {
                 return console.log(error);
             }
-            // Then split it by commas (to make it more readable)
-            console.log(data);
             var dataArr = data.split(",");
 
-            // We will then re-display the content as an array for later use.
-            console.log(dataArr);
-
             var action = dataArr[0];
-            var movieSong = dataArr[1];  
-
-            console.log(action);
-            console.log(movieSong);
-
+            var movieSong = dataArr[1];
+            //depending on the action readed this switch will execute a different function
             switch (action) {
                 case "my-tweets": {
                     tweet();
                     break;
                 }
                 case "spotify-this-song": {
-                    // var comand = dataArr[1];
-                    // var comandReplace = replaceName(comand);
-                    // console.log("this is comand " + comandReplace);
-                    spotifySong(dataArr[1]);
+                    spotifySong(movieSong, type);
                     break;
                 }
                 case "movie-this": {
-                    // var comandReplace = replaceName(comand);
-                    // console.log("this is comand " + comandReplace);
-                    movie(dataArr[1]);
+                    movie(movieSong);
                     break;
                 }
             }
         });
         break;
     }
+        //this function make a query to spotify api to retrieve information about a song
+        function spotifySong(song, type) {
 
-        function spotifySong(song) {
-
-            spotify.search({ type: 'track', query: songName }, function (error, song) {
+            spotify.search({ type: type, query: song }, function (error, song) {
                 console.log(error);
                 if (!error) {
                     console.log(song.tracks.items[0].artists[0].name);
@@ -115,17 +97,27 @@ switch (oper) {
             });
         }
 
+        //this function make a query to the omdb api in order to retrieve information about a specific movie
         function movie(movie) {
-            var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
+            var queryUrl = "http://www.omdbapi.com/?t=" + movie + "&y=&plot=short&apikey=trilogy";
 
             request(queryUrl, function (error, response, body) {
 
-                // If the request is successful
+                // If the request is successful show the information
                 if (!error && response.statusCode === 200) {
                     console.log("Title: " + JSON.parse(body).Title);
                     console.log("Release Year: " + JSON.parse(body).Year);
                     console.log("IMDB Rating: " + JSON.parse(body).imdbRating);
-                    console.log("Rotten Tomatoes: " + JSON.parse(body).Ratings[1].Value);
+
+                    // Some movies do not have rotten tomatoes, that's need to be checked
+                    var ratingFilm = JSON.parse(body).Ratings.length;  //wrap in a var the length of the rating
+                    for (let i = 0; i < ratingFilm; i++) { //loop the rating checking for source = rotten tomatoes
+                        if (JSON.parse(body).Ratings[i].Source === "Rotten Tomatoes") {
+                            console.log("Rotten Tomatoes: " + JSON.parse(body).Ratings[i].Value);  //if the movie has a value it's shown
+                        } else {
+                            console.log("Rotten Tomatoes: N/A"); //if not show N/A as value of the rotten tomatoes
+                        }
+                    }
                     console.log("Country: " + JSON.parse(body).Country);
                     console.log("Language: " + JSON.parse(body).Language);
                     console.log("Plot: " + JSON.parse(body).Plot);
@@ -134,25 +126,18 @@ switch (oper) {
             });
         }
 
+        // this function make a request to the Twitter api with a user_name and and "number" and return the last "number" of tweets of the user.
         function tweet() {
             client.get('statuses/user_timeline', { screen_name: 'iri04930287', count: 20 }, function (error, tweets, response) {
 
                 for (i = 0; i < tweets.length; i++) {
                     console.log('================')
                     console.log('')
-                    console.log('')
                     console.log(tweets[i].text);
-                    console.log('')
                     console.log('')
                     console.log('================')
                 }
             });
-        }
-
-
-        function replaceName(e) {
-            var son = e.replace(/ /g, "+");
-            return son;
         }
 }
 
